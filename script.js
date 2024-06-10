75f3b18bf02d55f7c7af25e196dbc5eda2ea9cb9
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-06-11 00:28:44 +0530

    .script

---
+++ script.js
@@ -0,0 +1,11 @@
+const loregeBox = document.querySelector(".lorege-box");
+const loginlink = document.querySelector(".login-link");
+const registerlink = document.querySelector(".register-link");
+
+registerlink.addEventListener("click", () => {
+    loregeBox.classList.add("active");
+});
+
+loginlink.addEventListener("click", () => {
+    loregeBox.classList.remove("active");
+});
